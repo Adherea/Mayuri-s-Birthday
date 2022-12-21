aef0050b485964dfa0328ed0f1463b1756c8367b
dityasismantoro <dityasismantoro@gmail.com>
2022-12-21 23:35:36 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("btn-el");
+const paragraf = document.getElementById("paragraf");
+
+button.addEventListener("click", function () {
+  if ((paragraf.innerText = true)) {
+    paragraf.innerText = "おめでとう！^^";
+  }
+});
